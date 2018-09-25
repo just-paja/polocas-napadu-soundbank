627e2579e051329fca53cb20b75d7ed331138cb6
Pavel Žák <pavel@berrycloud.cz>
2018-09-25 23:49:41 +0200

    Less forgiving tests!

--- validateModules.js
+++ validateModules.js
@@ -4,6 +4,7 @@ const { validateModule } = require('./schema');
 
 glob('./**/*.json', (err, files) => {
   let errors = 0;
+  let tags = [];
   if (err) {
     throw new Error(err);
   }
@@ -13,6 +14,34 @@ glob('./**/*.json', (err, files) => {
     if (validationResult.errors.length) {
       validationResult.errors.forEach(validationError => {
         console.error(`${file}: ${validationError.stack}`, validationResult.instance);
+      });
+      errors += 1;
+    } else {
+      if (fileContent.tags) {
+        tags = tags
+          .concat(fileContent.tags)
+          .filter((tag, index, self) => self.findIndex(tagB => tagB.name === tag.name) === index);
+      }
+    }
+  });
+  files.forEach((file) => {
+    const { baseTag, sounds } = require(file);
+    if (baseTag) {
+      if (!tags.find(tag => tag.name === baseTag)) {
+        console.error(`Missing baseTag definition for ${tagName} in module ${file}`);
+        errors += 1;
+      }
+    }
+    if (sounds) {
+      sounds.forEach((sound) => {
+        if (sound.tags) {
+          sound.tags.forEach((tagName) => {
+            if (!tags.find(tag => tag.name === tagName)) {
+              console.error(`Missing tag definition for ${tagName} on sound ${sound.file} in module ${file}`);
+              errors += 1;
+            }
+          });
+        }
       });
     }
   });
