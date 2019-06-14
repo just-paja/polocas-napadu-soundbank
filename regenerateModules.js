ef3a0163e3d4b0fdb0d56ebd304d97c7d27d8470
Pavel Žák <pavel@berrycloud.cz>
2019-06-14 17:51:37 +0200

    Sanitize sound names

--- regenerateModules.js
+++ regenerateModules.js
@@ -1,31 +1,80 @@
 const fs = require('fs')
 const glob = require('glob')
 const path = require('path')
+const deepEqual = require('deep-equal')
 
-function areArraysEqual (arr1, arr2) {
-  return (
-    arr1 &&
-    arr2 &&
-    arr1.length === arr2.length &&
-    arr1.every((u, i) => u === arr2[i])
+const isDirectory = source => fs.lstatSync(source).isDirectory()
+const getDirectories = source =>
+  fs.readdirSync(source).map(name => path.join(source, name)).filter(isDirectory)
+
+const getDirectoriesDeep = source => getDirectories(source)
+  .filter(dir => !dir.match(/(node_modules|^\.)/))
+  .reduce(
+    (aggr, dir) => [
+      ...aggr,
+      path.join(dir),
+      ...getDirectoriesDeep(dir)
+    ],
+    []
   )
+
+const getManifest = (dir, manifestPath) => {
+  try {
+    return {
+      exists: true,
+      manifest: require(manifestPath)
+    }
+  } catch (e) {
+    return {
+      exists: false,
+      manifest: {
+        name: dir
+      }
+    }
+  }
 }
 
-glob
-  .sync(path.join('.', '**', '*.json'))
-  .filter(file => !file.match(/node_modules/))
-  .forEach((file) => {
-    const manifestPath = path.resolve(__dirname, file)
-    const modulePath = path.resolve(__dirname, file, '..')
-    const manifest = require(manifestPath)
-    const sounds = glob.sync(path.join(modulePath, '*.mp3')).map(file => path.basename(file))
+const sanitizeName = (fileName) =>
+  fileName
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[\s_\-()]+/g, '-')
+    .replace(/[,]/g, '')
+    .replace(/^-/g, '')
 
-    if (!areArraysEqual(manifest.sounds, sounds)) {
-      process.stderr.write(`Regenerating ${file}\n`)
-      const nextManifest = {
-        ...manifest,
-        sounds
-      }
+getDirectoriesDeep('.')
+  .filter(dir => !dir.match(/(node_modules|^\.)/))
+  .map(dirName => ({
+    dirName,
+    manifestPath: path.resolve(__dirname, dirName, 'manifest.json'),
+    modulePath: path.resolve(__dirname, dirName)
+  }))
+  .forEach(({ dirName, manifestPath, modulePath }) => {
+    const { exists, manifest } = getManifest(dirName, manifestPath)
+    const sounds = glob.sync(path.join(modulePath, '*.mp3'))
+      .map(file => path.basename(file))
+      .map((file) => {
+        const soundNameSanitized = sanitizeName(file)
+        const soundPath = path.join(modulePath, file)
+        const soundPathSanitized = path.join(modulePath, soundNameSanitized)
+        if (soundPath !== soundPathSanitized) {
+          process.stderr.write(`Renaming "${file}" to "${soundNameSanitized}"\n`)
+          fs.renameSync(soundPath, soundPathSanitized)
+        }
+        return soundNameSanitized
+      })
+    const modules = getDirectories(modulePath)
+    const nextManifest = {
+      ...manifest,
+      modules,
+      sounds
+    }
+
+    // console.log(manifest, nextManifest)
+
+    if (!exists || !deepEqual(manifest.sounds, sounds)) {
+      process.stderr.write(`Regenerating ${dirName}\n`)
       const fd = fs.openSync(manifestPath, 'w')
       fs.writeSync(fd, JSON.stringify(nextManifest, null, 2))
       fs.closeSync(fd)
